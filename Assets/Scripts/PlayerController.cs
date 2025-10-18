972ec1eb52f7f20cec5eb3cc898faefbd31a68ff
markalmax <markalmax782@gmail.com>
2025-10-18 12:48:06 +0300

    reworked code
    tip to self: using AI was NOT the best idea

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,75 +3,81 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveForce = 400f, maxSpeed = 5f, maxGrappleDistance = 8f, reelSpeed = 3f;
-    public LayerMask grappleMask = -1;
-    Rigidbody2D rb;
-    LineRenderer lr;
-    DistanceJoint2D dj;
-    PlayerInput pi; 
-    InputAction moveA, attackA, lookA;
-    Vector2 move;
-    Vector2 grapplePoint;
-    bool grappling; 
+    [SerializeField]
+    public float moveSpeed = 10f;
+    public float maxVelocity = 10f;
+    private Rigidbody2D rb;
+    private LineRenderer lr;
+    private DistanceJoint2D dj;
+    private Vector2 moveInput;
+    private bool grappleInput;
+    private Vector2 mospos;
+    private PlayerInput playerInput;
+    private InputAction moveAction;
+    private InputAction grapple;
+
     void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        if (playerInput == null)
+        {
+            playerInput = gameObject.AddComponent<PlayerInput>();
+        }
+        moveAction = playerInput.actions["Move"];
+        grapple = playerInput.actions["Attack"];
+    }
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         lr = GetComponent<LineRenderer>();
         dj = GetComponent<DistanceJoint2D>();
-        pi = GetComponent<PlayerInput>();
-
-        moveA = pi.actions["Move"];
-        attackA = pi.actions["Attack"];
-        lookA = pi.actions["Look"];
-
-        lr.enabled = false;
-        dj.enabled = false; 
     }
-
-    void OnEnable() { moveA.Enable(); attackA.Enable(); lookA.Enable(); }
-    void OnDisable() { moveA.Disable(); attackA.Disable(); lookA.Disable(); }
-
+    void OnEnable()
+    {
+        moveAction.Enable();
+        grapple.Enable();
+        
+    }
+    void OnDisable()
+    {
+        moveAction.Disable();
+        grapple.Disable();
+    }
     void Update()
     {
-        float moveX = moveA.ReadValue<Vector2>().x;
-        move = new Vector2(moveX, 0);
-        bool attack = attackA.ReadValue<float>() > 0.5f;
-        if (attack)
-        {
-            var origin = (Vector2)transform.position;
-            Vector2 aim;
-            if (Mouse.current != null)
-                aim = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-            else
-            {
-                var stick = lookA.ReadValue<Vector2>();
-                aim = stick.sqrMagnitude > 0.01f ? origin + stick.normalized * maxGrappleDistance : origin + Vector2.right * maxGrappleDistance;
-            }
-            var dir = (aim - origin).normalized;
-            var hit = Physics2D.Raycast(origin, dir, maxGrappleDistance, grappleMask);
-            if (hit)
-            {
-                print("Grappled to " + hit.point);
-                grapplePoint = hit.point;
-                grappling = true;
-                lr.enabled = true;
-                dj.enabled = true;
-                dj.connectedAnchor = grapplePoint;
-                dj.distance = Vector2.Distance(origin, grapplePoint);
-            }
-        }
-        if (!attack) { grappling = false; lr.enabled = false; dj.enabled = false; }
-
+        moveInput = moveAction.ReadValue<Vector2>();
+        grappleInput = grapple.ReadValue<float>() > 0.5f;
+        mospos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
     }
-
     void FixedUpdate()
     {
-        rb.AddForce(new Vector2(move.x, 0) * moveForce * Time.fixedDeltaTime);
-        if (rb.linearVelocity.magnitude > maxSpeed) rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
-        if (grappling)
+        if (moveInput != Vector2.zero)
         {
-            lr.SetPosition(0, transform.position); lr.SetPosition(1, grapplePoint);
-            if (dj.distance > 0.5f) dj.distance = Mathf.Max(0.5f, dj.distance - reelSpeed * Time.fixedDeltaTime);
+            rb.AddForce(moveInput.normalized * moveSpeed);
+            if (rb.linearVelocity.magnitude > maxVelocity)
+            {
+                rb.linearVelocity = rb.linearVelocity.normalized * maxVelocity;
+            }
+        }
+        if (grappleInput)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, mospos - (Vector2)transform.position, Mathf.Infinity, LayerMask.GetMask("Ground"));
+            if (hit)
+            {
+                dj.enabled = true;
+                dj.connectedAnchor = hit.point;
+                dj.distance = Vector2.Distance(transform.position, hit.point);
+                lr.enabled = true;
+                lr.positionCount = 2;
+                lr.SetPosition(0, transform.position);
+                lr.SetPosition(1, hit.point);
+            }
+        }
+        else
+        {
+            dj.enabled = false;
+            lr.enabled = false;
+            lr.positionCount = 0;
         }
     }
 }
