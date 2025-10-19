6a9973da8828c0efac24b6cee65ae25e77048cf0
markalmax <markalmax782@gmail.com>
2025-10-19 23:16:35 +0300

    Finalized mainmenu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,21 +2,29 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    public GameObject main;
+    public GameObject levels;  
+    public GameObject info;
     public void StartGame()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
     }
     public void levelsMenu()
     {
-        
+        main.SetActive(false);
+        levels.SetActive(true);
     }
     public void InfoMenu()
     {
-
+        main.SetActive(false);
+        info.SetActive(true);
     }
     public void BackMenu()
     {
-        
+        main.SetActive(true);
+        levels.SetActive(false);
+        info.SetActive(false);
     }
     public void QuitGame()
     {
