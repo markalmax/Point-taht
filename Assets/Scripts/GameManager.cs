2a7c7d7e4fde5185c1ebc06770aacda88040b845
markalmax <markalmax782@gmail.com>
2025-10-30 20:52:56 +0300

    added countdoiwn at level start

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public float timer = 0f;
+    public float startWait = 3f;
     public Rigidbody2D rb;
     public TMP_Text timerText;
     public TMP_Text highScoreText;
     public TMP_Text speedText;
+    public TMP_Text startText;
+    private bool flag;
+    private float startTime;
     void Start()
     {
+        startTime = startWait;
         rb=FindFirstObjectByType<Rigidbody2D>();
         if (PlayerPrefs.HasKey("HighScore"))
         {
@@ -30,9 +35,25 @@ public class GameManager : MonoBehaviour
     }
     void FixedUpdate()
     {
+        if (startTime <= 0f)
+        {
+            flag = true;
+            startText.text = "Go!";
+        }
+        else
+        {
+            startTime -= Time.deltaTime;
+            string timeDisplay = startTime.ToString("F0");
+            if (timeDisplay == "0")timeDisplay = "Go!";
+            startText.text = timeDisplay;
+        }
+        if(flag)timer += Time.deltaTime;
         speedText.text = rb.linearVelocity.magnitude.ToString("F1") + " Unit/Sec";
-        timer += Time.deltaTime;
         TimeSpan time = TimeSpan.FromSeconds(timer);
+        if (time.TotalSeconds >= 1)
+        {
+            startText.gameObject.SetActive(false);
+        }
         timerText.text = time.ToString(@"mm\:ss\:fff");
     }
 }
