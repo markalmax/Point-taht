1ced2dfddcb94211d0f7e033578d38a3eb8dab31
markalmax <markalmax782@gmail.com>
2026-01-17 22:29:34 +0200

    updated squash and stretch

--- Assets/Scripts/SquashAndStretch.cs
+++ Assets/Scripts/SquashAndStretch.cs
@@ -5,6 +5,7 @@ public class SquashAndStretch : MonoBehaviour
     public Rigidbody2D rb;
     public PlayerController pc;
     private Vector2 contactSquash;
+    private Vector2 lastFrameVelocity;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -18,8 +19,23 @@ public class SquashAndStretch : MonoBehaviour
         float ty = Mathf.Clamp01(Mathf.Abs(hv) / pc.maxVelocity);
         float finalX = 1f + tx - (0.5f * ty);
         float finalY = 1f + ty - (0.5f * tx);
+        finalX *= contactSquash.x;
+        finalY *= contactSquash.y;
+        contactSquash = Vector2.Lerp(contactSquash, Vector2.one, 0.15f);
         transform.localScale = new Vector3(finalX, finalY, 1f);
-        Debug.Log("HH: " + hh + " HV: " + hv);
+        //Debug.Log("HH: " + hh + " HV: " + hv);
+        lastFrameVelocity = rb.linearVelocity;
     }
-    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("CV: " + lastFrameVelocity+" LV: " + rb.linearVelocity);
+        if(rb.linearVelocityX == 0 || rb.linearVelocityY == 0)
+        {
+            float collisionStrengthX = Mathf.Clamp01(Mathf.Abs(lastFrameVelocity.x) / pc.maxVelocity);
+            float collisionStrengthY = Mathf.Clamp01(Mathf.Abs(lastFrameVelocity.y) / pc.maxVelocity);
+            float squashX = 1f - (collisionStrengthY * 0.5f);
+            float stretchY = 1f + (collisionStrengthX * 0.5f);
+            contactSquash = new Vector2(squashX, stretchY);
+        }
+    }
 }
