40da35b505739dcf30f31155f06f23afe7c8d4bf
markalmax <markalmax782@gmail.com>
2026-01-23 20:52:51 +0200

    added cute faces to the charcter

---
+++ Assets/Scripts/SpriteControlller.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+
+public class SpriteControlller : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public SpriteRenderer sr;
+    public float threshold=1f; 
+    public Sprite Smile;
+    public Sprite Flat;
+    public Sprite Frown;
+    void Start()
+    {
+        rb=GetComponent<Rigidbody2D>();
+        sr=GetComponent<SpriteRenderer>();
+    }
+    void Update()
+    {
+        if (sr.sprite != Frown)
+        {
+            if (rb.linearVelocity.magnitude >= threshold)sr.sprite=Smile;
+            else sr.sprite=Flat;
+        }
+    }
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(!isGrounded())StartCoroutine(ShowSadSprite());
+    }
+    IEnumerator ShowSadSprite()
+    {
+        sr.sprite = Frown;
+        yield return new WaitForSeconds(1f);
+        sr.sprite = Flat;
+    }
+    public bool isGrounded() { return Physics2D.Raycast(transform.position, Vector2.down, 1.1f, LayerMask.GetMask("Ground")).collider != null; }
+}
